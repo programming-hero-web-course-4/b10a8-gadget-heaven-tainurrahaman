bdfadac6f24baeb25269bb61cd604ac57083f1d4
tainur <tainurrahaman31@gmail.com>
2024-11-26 22:03:45 +0600

    complete home page and made responsive for all devices

--- src/components/AllGadgets.jsx
+++ src/components/AllGadgets.jsx
@@ -12,20 +12,34 @@ const AllGadgets = () => {
 
   return (
     <div className="bg-[#1D232A] pt-[125px] md:pt-80 lg:pt-96 pb-10 md:pb-16 lg:pb-24">
-      <h3 className="font-bold text-[40px] text-white text-center mb-8">
+      <h3 className="font-bold text-[22px] md:text-[40px] text-white text-center mb-8">
         Explore Cutting-Edge Gadgets
       </h3>
-      <div className="w-10/12 mx-auto flex">
-        <div className="w-1/5 ">
-          <p>All Product</p>
-          <p>Laptops</p>
-          <p>Phones</p>
-          <p>Accessories</p>
-          <p>Smart Watches</p>
-          <p>MacBook</p>
-          <p>Iphone</p>
+      <div className="w-full md:w-11/12 lg:w-10/12 mx-auto flex gap-2 md:gap-6">
+        <div className="w-1/5 md:w-1/6 lg:w-1/5 p-1 md:p-2 lg:p-6 bg-[#403546] rounded-xl md:rounded-2xl space-y-5 text-center lg:text-start">
+          <p className="rounded-3xl bg-[#1D232A] p-2 md:py-3 md:px-3 lg:px-5 font-normal md:font-medium text-[12px] md:text-[16px] lg:text-[18px]">
+            All Product
+          </p>
+          <p className="rounded-3xl bg-[#1D232A] p-2 md:py-3 md:px-3 lg:px-5 font-normal md:font-medium text-[12px] md:text-[16px] lg:text-[18px]">
+            Laptops
+          </p>
+          <p className="rounded-3xl bg-[#1D232A] p-2 md:py-3 md:px-3 lg:px-5 font-normal md:font-medium text-[12px] md:text-[16px] lg:text-[18px]">
+            Phones
+          </p>
+          <p className="rounded-3xl bg-[#1D232A] p-2 md:py-3 md:px-3 lg:px-5 font-normal md:font-medium text-[12px] md:text-[16px] lg:text-[18px]">
+            Accessories
+          </p>
+          <p className="rounded-3xl bg-[#1D232A] p-2 md:py-3 md:px-3 lg:px-5 font-normal md:font-medium text-[12px] md:text-[16px] lg:text-[18px]">
+            Smart Watches
+          </p>
+          <p className="rounded-3xl bg-[#1D232A] p-2 md:py-3 md:px-3 lg:px-5 font-normal md:font-medium text-[12px] md:text-[16px] lg:text-[18px]">
+            MacBook
+          </p>
+          <p className="rounded-3xl bg-[#1D232A] p-2 md:py-3 md:px-3 lg:px-5 font-normal md:font-medium text-[12px] md:text-[16px] lg:text-[18px]">
+            Iphone
+          </p>
         </div>
-        <div className="w-4/5 grid grid-cols-1 md:grid-cols-3 gap-6">
+        <div className="w-4/5 md:w-5/6 lg:w-4/5 grid grid-cols-2 md:grid-cols-3 gap-2 md:gap-4 lg:gap-6">
           {gadgets.map((gadget) => (
             <GadgetCart key={gadget.product_id} gadget={gadget}></GadgetCart>
           ))}
--- src/components/GadgetCart.jsx
+++ src/components/GadgetCart.jsx
@@ -2,19 +2,23 @@ const GadgetCart = ({ gadget }) => {
   console.log(gadget);
   const { product_image } = gadget;
   return (
-    <div className="card bg-base-100 shadow-xl p-5">
+    <div className="card bg-[#403546] shadow-xl p-2 md:p-3 lg:p-5">
       <figure className="">
         <img
           src={product_image}
           alt="Shoes"
-          className="rounded-xl h-[200px] w-[280px] "
+          className="rounded-xl h-[100px] md:h-[160px] lg:h-[200px] w-[170px] md:w-[280px] "
         />
       </figure>
-      <div className="card-body items-center text-center">
-        <h2 className="card-title">Shoes!</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
-        <div className="card-actions">
-          <button className="btn btn-primary">Buy Now</button>
+      <div className="pt-3 md:pt-5 items-center space-y-2 md:space-y-3">
+        <h2 className="card-title font-semibold text-[20px] md:text-[24px]">
+          Shoes!
+        </h2>
+        <p className="font-medium text-[14px] md:text-[16px]">
+          If a dog chews shoes whose shoes does he choose?
+        </p>
+        <div className="card-actions justify-center">
+          <button className="btn btn-primary">View Details</button>
         </div>
       </div>
     </div>
